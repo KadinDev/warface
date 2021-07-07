ae887d436b06cb38add231619999a9a038b62d30
kadindev <developerwebmobile@outlook.com>
2021-07-07 18:48:30 -0300

    mudanças

---
+++ public/scripts/index.js
@@ -0,0 +1,30 @@
+const audio = document.querySelector('audio');
+const span_audio = document.querySelector('.box-audio span');
+
+const play = '<i class="fa fa-play-circle"></i>';
+const pause = '<i class="fa fa-pause-circle"></i>';
+
+span_audio.innerHTML = play
+
+span_audio.addEventListener('click', ()=>{
+    audio.volume = 0.5;
+    audio.play();
+    
+    if (span_audio.innerHTML === play) {
+        span_audio.innerHTML = pause;
+    } else {
+        span_audio.innerHTML = play;
+        audio.pause();
+    }
+    
+    span_audio.classList.toggle('active')
+})
+
+/*
+document.addEventListener('click', function(){
+    const audio = document.querySelector('audio');
+    audio.volume = 0.1;
+    audio.play();
+})
+*/
+
