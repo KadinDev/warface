bc7bfcf24dcda729a7154a8f92230f24e93a4fac
kadindev <developerwebmobile@outlook.com>
2021-07-10 22:41:52 -0300

    in progress

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -20,5 +20,29 @@ span_audio.addEventListener('click', ()=>{
     span_audio.classList.toggle('active')
 })
 
+// MODAL CLASSES PLAYERS
+const iframe = document.querySelectorAll('.videos-container .iframe');
+const video_classesPlayers = document.querySelector('.modal-videos');
+const close_videoClassesPlayers = video_classesPlayers.querySelector('i');
+
+for ( let video of iframe ) {
+    video.addEventListener('click', () => {
+        video_classesPlayers.classList.add('active');
+        const title = video.querySelector('h1').innerHTML;
+        const about = video.querySelector('p').innerHTML;
+
+        const src = video.getAttribute('id');
+
+        video_classesPlayers.querySelector('h1').innerHTML = title;
+        video_classesPlayers.querySelector('p').innerHTML = about;
+
+        video_classesPlayers.querySelector('iframe').src = `https://www.youtube.com/embed/${src}`;
+    });
+
+    close_videoClassesPlayers.addEventListener('click', () => {
+        video_classesPlayers.classList.remove('active');
+        video_classesPlayers.querySelector('iframe').src = "";
+    })
+}
 
 
